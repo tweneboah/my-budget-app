cc5f16cababef4ec9b6da5ad727f62458d4f7867
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-08-14 17:57:04 +0800

    v2

--- my-budget-app-v2/app.js
+++ my-budget-app-v2/app.js
@@ -13,12 +13,30 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.set("view engine", "ejs");
 
 
-
+//=============
+// HOME PAGE
 app.get('/', (req, res) => {
     res.render('index')
 })
 
+//=================
+// CREATE A BUGET
+//=================
 
+//1. Get the budget form
+app.get('/budget/new', (req, res) => {
+     res.render('createBudget')
+})
+
+//.2 Create budget Logic
+
+app.post('/budget', (req, res) => {
+    
+})
+
+//===============
+// USERS REGISTRATION
+//===============
 
 
 
